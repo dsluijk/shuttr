9517ffbcf607354e2cebcf281074c7140de80c7b
Dany Sluijk <me@dany.dev>
2025-11-09 20:16:18 +0100

    feat(database): allow connection with split up postgres values

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   out: "./server/database/migrations",
   extensionsFilters: ["postgis"],
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST ?? "localhost",
+    port: Number(process.env.DATABASE_HOST) || 5432,
+    database: process.env.DATABASE_DB ?? "shuttr",
+    user: process.env.DATABASE_USER ?? "",
+    password: process.env.DATABASE_PASSWORD ?? "",
   },
 });
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -7,7 +7,14 @@ let db: ReturnType<typeof drizzle<typeof schema>> | null;
 export function useDrizzle() {
   if (!db) {
     db = drizzle({
-      connection: { connectionString: process.env.DATABASE_URL },
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_HOST) || undefined,
+        database: process.env.DATABASE_DB,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+      },
       schema,
     });
   }
