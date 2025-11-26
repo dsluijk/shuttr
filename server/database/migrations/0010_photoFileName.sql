2b26538f1f5bdd675930db1e073a2663c389ad90
Dany Sluijk <me@dany.dev>
2025-11-26 20:06:21 +0100

    fix: correct invalid string in migration

--- server/database/migrations/0010_photoFileName.sql
+++ server/database/migrations/0010_photoFileName.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "photo" ADD COLUMN "fileName" text;--> statement-breakpoint
-UPDATE "photo" SET "fileName" = "unknown.jpg";--> statement-breakpoint
+UPDATE "photo" SET "fileName" = 'unknown.jpg';--> statement-breakpoint
 ALTER TABLE "photo" ALTER COLUMN "fileName" SET NOT NULL;
